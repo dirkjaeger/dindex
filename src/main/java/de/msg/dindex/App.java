7ca5bd58d8bd90697deaf220a194833deec37fe6
developer <developer@hackathon>
2018-11-23 14:13:08 +0100

    Erster Commit

--- src/main/java/de/msg/dindex/App.java
+++ src/main/java/de/msg/dindex/App.java
@@ -1,15 +1,47 @@
 package de.msg.dindex;
 
+import java.util.HashMap;
+
 import de.msg.dindex.crawler.Controller;
 
 public class App {
+	
+	private HashMap staedteUrl = new HashMap<String, String>();
+	
+	
 
+	public HashMap getStaedteUrl() {
+		return staedteUrl;
+	}
+	public void setStaedteUrl(HashMap staedteUrl) {
+		this.staedteUrl = staedteUrl;
+	}
+	
+	public HashMap intialisierteHashMap () {
+		
+		staedteUrl.put("Köln", "https://www.stadt-koeln.de/");
+		staedteUrl.put("Essen", "https://www.essen.de/aktuell/portalaktuell.de.html");
+		staedteUrl.put ("München", "https://www.muenchen.de/");
+		staedteUrl.put ("Düsseldorf", "https://www.duesseldorf.de/");
+		
+		return staedteUrl; 
+		
+	}
+	public String liefereUrl(String stadt) {
+		
+		return (String) staedteUrl.get(stadt);
+	}
+	
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
 		Controller controller = new Controller();
-		
 		controller.run();
+		
+		
+
+		
+	
 	}
 
 }
