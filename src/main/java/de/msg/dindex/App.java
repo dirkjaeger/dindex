36e71178ed4a2ca954741670c80453168a302b1a
developer <developer@hackathon>
2018-11-23 12:12:13 +0100

    initial

---
+++ src/main/java/de/msg/dindex/App.java
@@ -0,0 +1,15 @@
+package de.msg.dindex;
+
+import de.msg.dindex.crawler.Controller;
+
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Controller controller = new Controller();
+		
+		controller.run();
+	}
+
+}
